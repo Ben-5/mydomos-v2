1c001891f09a16270ae4f92360047162dd67efbf
marialena gallizzi <marialena@wanadoo.fr>
2020-03-12 10:24:49 +0100

    boulette , confondu page book/basket= REPAREE

--- reactapp/src/screens/Book.js
+++ reactapp/src/screens/Book.js
@@ -63,31 +63,6 @@ function Book(props){
         }
     }
 
-
-   const order = info.map((data,i) => {
-        var styleInclu = {};
-        if(data.opt === "XXe" || data.opt === "XVIIIe" ){
-        styleInclu = {display: "none"} 
-        }
-        return (
-            <div key={i} className="grid-container-book" style={{borderTop :"solid 1px #B5ACAC"}}>
-
-            <div className="grid-item-book book-date">
-                <div className="book-date"><Text text={new Date(data.date).toLocaleDateString('fr-FR', options)}/></div>
-                <div className="book-time"><Text text={data.time}/></div>
-                <div className="book-time"><Text text={data.lang}/></div>
-                {/* {optList} */}
-                <div className="book-time"><Text style={styleInclu} text={data.opt.join(" / ")}/></div>
-                <div className="book-stock"><Text text={stock}/></div>
-            </div>
-                <div><Text text={`${data.price} €`}/></div>
-                <div className="grid-item-book book-ticket"><InputNumber min={1} max={data.maxStock} defaultValue={1} onChange={e=>setQuantity(e)} value={quantity}/></div>
-                <div className="grid-item-book book-button"><Button buttonTitle="Valider" onClick={ () => {handleAdd(visit, data); goToBasket()}}/></div>
-        </div>
-        )
-    
-    })
-
   return(
 
     <div className="background">
@@ -118,7 +93,25 @@ function Book(props){
             <div className="account-subtitle">
                 <Subtitle subtitle={`Les visites disponibles pour ${visit.title}`}/>
             </div>
-                    {order}
+            {info.map((data,i) => (
+
+                <div key={i} className="grid-container-book" style={{borderTop :"solid 1px #B5ACAC"}}>
+
+                    <div className="grid-item-book book-date">
+                        <div className="book-date"><Text text={new Date(data.date).toLocaleDateString('fr-FR', options)}/></div>
+                        <div className="book-time"><Text text={data.time}/></div>
+                        <div className="book-time"><Text text={data.lang}/></div>
+                        <div className="book-time"><Text text={data.opt.join(" / ")}/></div>
+                        <div className="book-stock"><p className="book-stock">
+                            { data.stock < 3 ?  `Il ne reste que ${data.stock} places` : ""}
+                        </p></div>
+                        
+                    </div>
+                        <div><Text text={`${data.price} €`}/></div>
+                        <div className="grid-item-book book-ticket"><InputNumber min={1} max={data.maxStock} defaultValue={1} onChange={e=>setQuantity(e)} value={quantity}/></div>
+                        <div className="grid-item-book book-button"><Button buttonTitle="Valider" onClick={ () => {handleAdd(visit, data); goToBasket()}}/></div>
+                </div>
+                ))}
                 
         </div>
 
